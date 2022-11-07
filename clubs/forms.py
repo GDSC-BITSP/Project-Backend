ef87574f092f0159cf1684143d681e1bd82236b1
Yuvraj Doshi <f20212425@pilani.bits-pilani.ac.in>
2022-11-07 19:53:34 +0530

    Serializers
    some bugs fixed

--- clubs/forms.py
+++ clubs/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Club
 class ClubForm(forms.ModelForm):
     class Meta:
-        model = ClubForm
+        model = Club
         fields = {
             'author',
             'name',
